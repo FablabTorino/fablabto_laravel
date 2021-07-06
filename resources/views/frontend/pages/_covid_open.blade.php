ff63e379a780f07d6a25ab749de7ce98ab43e46b
Shad0w82 <gianfry.c@gmail.com>
2021-07-06 17:30:31 +0200

    Added new popup for Covid rules

---
+++ resources/views/frontend/pages/_covid_open.blade.php
@@ -0,0 +1,40 @@
+<div class="container simple_info">
+    <h2 class="content-title text-center text-danger" data-animation="true" data-animation-type="fadeInRight">
+        FAQ E REGOLE PER L'ACCESSO AL LABORATORIO</h2>
+
+    <p class="row">
+        <p class="col-md-12">
+
+            <p><strong class="text-danger">IL FABLAB È APERTO ?</strong><br>
+                Il laboratorio è aperto e utilizzabile dai soci tesserati nei consueti orari di apertura (lun. - ven. 16.00 - 20.00),
+                previa prenotazione e senza superare la capienza massima di 15 persone.
+
+            <p>Per prenotare &egrave; necessario:<br/></p>
+                <ul>
+                    <li>Controllare le disponibilit&agrave; sul <a href="http://bit.ly/calendarioprenotazioni_fabto">Calendario Prenotazioni</a></li>
+                    <li>Compilare il <a href="http://bit.ly/formprenotazione_fabto">Form di Prenotazione</a></li>
+                </ul>
+
+            <p>Queste regole sono valide solo ed esclusivamente per i soci tesserati.</p>
+
+            <p><strong class="text-danger">E' POSSIBILE PARTECIPARE ALL'OPEN DAY?</strong><br>
+                Per i non tesserati, gli Open Day sono come sempre al mercoledì (esclusi i festivi) dalle 19.00 alle 20.00,
+                su prenotazione inviando una mail a <a href="mailto:info@fablabtorino.org">info@fablabtorino.org</a> </p>
+
+            <p>Per ovvie ragioni, ci &egrave; possibile ospitare gli Open Day solo nei mercoled&igrave; in cui il Piemonte
+                &egrave; Area Gialla. Se nel giorno prenotato la regione dovesse passare ad Area Arancione o Rossa, il
+                laboratorio rester&agrave; chiuso e le prenotazioni verranno automaticamente considerate nulle.</p>
+
+            <p><strong class="text-danger">AVETE IN PROGRAMMA WORKSHOP IN PRESENZA?</strong><br>
+                Da maggio la nostra offerta formativa è di nuovo in presenza negli spazi del Fablab,
+                nel pieno rispetto delle norme di sicurezza.</p>
+            <p>Per conoscere le date dei prossimi workshop assicurati di seguirci su <a href="https://www.facebook.com/fablabtorino">Facebook</a>,
+                di essere iscritto alla nostra <a href="index.html#contact" onclick="$('#modal-covid').modal('hide')" >Newsletter</a> e
+                di seguire il canale <a href="https://t.me/fablabtorino">Telegram News</a></p>
+
+            <p> Ci vediamo al Fablab! </p>
+
+        </p>
+    </p>
+</div>
+<!-- end container -->
